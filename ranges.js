9cddf38506ab3b0d0d135b01627e10840544ff4f
Dominic Tarr <dominic.tarr@gmail.com>
2014-04-14 20:50:51 +1000

    correctly hash things!

--- ranges.js
+++ ranges.js
@@ -3,23 +3,24 @@ var createHash = require('crypto').createHash
 var stringify = require('json-stable-stringify')
 
 module.exports = function (size, ts) {
-  var start, current, hash
+  var start, current, hash, count = 0
   if('function' !== typeof ts)
     throw new Error('ts(obj) must be provided')
   if(isNaN(size) || size < 0)
     throw new Error('size must be a positive integer')
-
+  hash = createHash('sha256')
   return through(function (data) {
     var t = ts(data)
     if(!start)
       current = start = t - t%size
 
     if(t > current + size) {
-      this.queue({start: current, length: size, hash: hash.digest('hex')})
+      this.queue({start: current, length: size, hash: hash.digest('hex'), count: count})
       current += size
+      hash = createHash('sha256')
+      count = 0
     }
-    console.log(current, data)
-    hash = createHash('sha256')
+    count ++
     hash.update(stringify(data) + '\n', 'ascii')
 
   }, function () {
