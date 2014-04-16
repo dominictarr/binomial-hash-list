dab8ecbbcee7f3a7b4937645eb962158bbadf747
Dominic Tarr <dominic.tarr@gmail.com>
2014-04-16 20:58:05 +1000

    tidy

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// createRead(opts)
-//
-// must return a readable pull-stream, and take
-// start and end, which should be unix timestamps.
-
-// createWrite(cb)
-//
-// must return a reader pull-stream and take
-// a cb, this is called when all records have been written.
-// * maybe this could be a transform stream, and then the replication can be 2 ways.
-
 var pull      = require('pull-stream')
 var handshake = require('pull-handshake')
 var reduce    = require('./reduce')
@@ -31,23 +20,38 @@ function onEnd(end_cb) {
   }
 }
 
+// createRead(opts)
+//
+// must return a readable pull-stream, and take
+// start and end, which should be unix timestamps.
+
+// createWrite(cb)
+//
+// must return a reader pull-stream and take
+// a cb, this is called when all records have been written.
+// * maybe this could be a transform stream, and then the replication can be 2 ways.
+
 module.exports = function (opts, cb) {
-  var type = opts.server ? 'server' : 'client'
   return handshake(function (cb) {
-    pull(opts.read(), ranges(opts.size, opts.ts), reduce(function (err, tree) {
-      console.log('TREE-' + type.toUpperCase(), tree)
-      cb(err, tree)
-    }))
+    pull(
+      opts.read(),
+      ranges(opts.size, opts.ts),
+      reduce(function (err, tree) {
+        cb(err, tree)
+      })
+    )
   }, function (mine, yours) {
     if(!opts.server) return { sink: opts.write(cb), source: pull.defer()}
     var missing = compare(mine, yours)
-    console.log(missing)
     return {
-      source: pull(cat(missing.reverse().map(function(range) {
-        return opts.read({gte: range.start, lt: range.start + range.length})
-      })), onEnd(cb || function () {})),
+      source: pull(
+        cat(missing.reverse()
+          .map(function(range) {
+            return opts.read({gte: range.start, lt: range.start + range.length})
+          })),
+        onEnd(cb || function () {})
+      ),
       sink: pull.drain(null, cb)
     }
   })
-
 }
